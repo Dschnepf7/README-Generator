391d99326eef6a220c0a7c0ab26ad0624f3c3ee5
Dschnepf7 <domschnepf1@gmail.com>
2023-02-16 14:36:08 -0600

    uninstalled inquirer and reinstalled correct version, ran first test

--- Assets/README.md
+++ Assets/README.md
@@ -1,2 +1,34 @@
-# README-Generator
-A program designed to generate a readme file based on user inputs prompted by Inquirer
+
+# Test
+
+## Description
+
+This is a test of the readme generator
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+
+## Installation
+
+requires installation of inquirer ver 8.2.4
+
+## Usage
+
+Test information
+
+## Contributing
+
+Email for info about contribution domschnepf1@gmail.com
+
+## Tests
+
+test instructions
+
+## License
+
+This project is licensed under the MIT license.
--- Assets/index.js
+++ Assets/index.js
@@ -2,48 +2,48 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: "What is your project's title?",
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Please provide a brief description of your project:',
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'What are the installation instructions for your project?',
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'What is the usage information for your project?',
-        },
-        {
-            type: 'input',
-            name: 'contributing',
-            message: 'What are the contribution guidelines for your project?',
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'What are the test instructions for your project?',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'What kind of license does your project have?',
-            choices: ['MIT', 'GPLv3', 'Apache', 'Unlicense', 'Other'],
-        },
-    ])
-    .then((answers) => {
-        const filename = 'README.md';
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: "What is your project's title?",
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a brief description of your project:',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the installation instructions for your project?',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What is the usage information for your project?',
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'What are the contribution guidelines for your project?',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What are the test instructions for your project?',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'What kind of license does your project have?',
+      choices: ['MIT', 'GPLv3', 'Apache', 'Unlicense', 'Other'],
+    },
+  ])
+  .then((answers) => {
+    const filename = 'README.md';
 
-        const content = `
+    const content = `
 # ${answers.title}
 
 ## Description
@@ -79,11 +79,11 @@ ${answers.tests}
 This project is licensed under the ${answers.license} license.
 `;
 
-        fs.writeFile(filename, content, (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log(`README.md successfully generated!`);
-            }
-        });
+    fs.writeFile(filename, content, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`README.md successfully generated!`);
+      }
     });
+  });
