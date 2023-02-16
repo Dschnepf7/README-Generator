93faa1581810d369c4a408fb8ac9fc37ccd5d5ac
Dschnepf7 <domschnepf1@gmail.com>
2023-02-16 14:21:21 -0600

    added content of readme.md file that is to be generated and function to write the file

--- Assets/index.js
+++ Assets/index.js
@@ -2,41 +2,88 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: "What is your project's title?",
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Please provide a brief description of your project:',
-    },
-    {
-      type: 'input',
-      name: 'installation',
-      message: 'What are the installation instructions for your project?',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'What is the usage information for your project?',
-    },
-    {
-      type: 'input',
-      name: 'contributing',
-      message: 'What are the contribution guidelines for your project?',
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'What are the test instructions for your project?',
-    },
-    {
-      type: 'list',
-      name: 'license',
-      message: 'What kind of license does your project have?',
-      choices: ['MIT', 'GPLv3', 'Apache', 'Unlicense', 'Other'],
-    },
-  ])
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: "What is your project's title?",
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a brief description of your project:',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the installation instructions for your project?',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the usage information for your project?',
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'What are the contribution guidelines for your project?',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What are the test instructions for your project?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What kind of license does your project have?',
+            choices: ['MIT', 'GPLv3', 'Apache', 'Unlicense', 'Other'],
+        },
+    ])
+    .then((answers) => {
+        const filename = 'README.md';
+
+        const content = `
+# ${answers.title}
+
+## Description
+
+${answers.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+
+## Installation
+
+${answers.installation}
+
+## Usage
+
+${answers.usage}
+
+## Contributing
+
+${answers.contributing}
+
+## Tests
+
+${answers.tests}
+
+## License
+
+This project is licensed under the ${answers.license} license.
+`;
+
+        fs.writeFile(filename, content, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(`README.md successfully generated!`);
+            }
+        });
+    });
